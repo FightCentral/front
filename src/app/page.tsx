0be3b24372b2741678cacb0884add2fe2dc82461
Pierre-Joseph Beaugendre <55739114+pjbeaugendre@users.noreply.github.com>
2024-10-13 11:41:20 +0200

    feat: colors and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Nous partons pour la bagarre
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col row-start-2 items-center sm:items-start">
+        <span className="font-[family-name:var(--font-sternbach)] text-5xl font-bold italic">FIGHT CENTRAL</span>
+        <span className="font-[family-name:var(--font-euro-ex)] text-[#D20A0A] font-thin text-4xl italic">CHAMPION</span>
+        <span className="font-[family-name:var(--font-euro-ex)] text- font-normal tracking-wide leading-6">
+        L'Ultimate Fighting Championship (UFC) est une organisation américaine d'arts martiaux mixtes (MMA), actuellement reconnue comme la plus importante ligue mondiale de ce sport de combat. La première soirée tenue par la promotion en novembre 1993 à Denver dans le Colorado, se présente comme un tournoi permettant de confronter différents styles d'arts martiaux et sports de combat dans des règles libres. Cet événement est alors souvent considéré comme un pilier de l'avènement des « MMA modernes ».
+        </span>
+        
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <div>
