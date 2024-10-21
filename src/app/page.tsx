2b953030ed836b9c11026c26a9cab2e64417bd96
Theo Poette <theo.poette@kbrwadventure.comm>
2024-10-21 09:43:56 +0200

    FIX: reset update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <div>
-          Nous avons gagné la bagarre :)
+          Nous avons gagné la bagarre
         </div>
       </footer>
     </div>
